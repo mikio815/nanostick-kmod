50e98d8e554dd3155259620716344dadbe7c38c6
mikio <uaru0815@gmail.com>
2026-01-07 11:46:13 +0900

    カーネルバージョン違いを吸収

--- ns_ldisc.c
+++ ns_ldisc.c
@@ -1,6 +1,7 @@
 #include <linux/tty.h>
 #include <linux/tty_ldisc.h>
 #include <linux/kernel.h>
+#include <linux/version.h>
 #include "ns_ldisc.h"
 
 static int ns_ldisc_open(struct tty_struct *tty) {
@@ -19,6 +20,9 @@ static void ns_ldisc_receive_buf(struct tty_struct *tty, const u8 *cp, const u8 
 static struct tty_ldisc_ops ns_ldisc_ops = {
     .owner = THIS_MODULE,
     .name = "ns_ldisc",
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(6,0,0)
+    .num = NS_LDISC_ID,
+#endif
     .open = ns_ldisc_open,
     .close = ns_ldisc_close,
     .receive_buf = ns_ldisc_receive_buf,
@@ -26,10 +30,18 @@ static struct tty_ldisc_ops ns_ldisc_ops = {
 
 int ns_ldisc_init(void)
 {
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(6,0,0)
+    return tty_register_ldisc(&ns_ldisc_ops);
+#else
     return tty_register_ldisc(NS_LDISC_ID, &ns_ldisc_ops);
+#endif
 }
 
 void ns_ldisc_exit(void)
 {
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(6,0,0)
+    tty_unregister_ldisc(&ns_ldisc_ops);
+#else
     tty_unregister_ldisc(NS_LDISC_ID);
+#endif
 }
